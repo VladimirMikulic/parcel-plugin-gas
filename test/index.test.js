07723f44bc8dcc5d55f53fa73a1c70046de454eb
Vladimir <vladimir.mikulic2@gmail.com>
2021-05-27 22:01:59 +0200

    UPDATE: Dependencies

--- test/index.test.js
+++ test/index.test.js
@@ -1,18 +1,18 @@
-var fs = require('fs');
-const path = require('path');
-const Bundler = require('parcel-bundler');
-const parcelPluginGAS = require('../index');
-const gasEntryGenerator = require('gas-entry-generator');
+var fs = require("fs");
+const path = require("path");
+const Bundler = require("parcel-bundler");
+const parcelPluginGAS = require("../lib/index");
+const generateEntryFunctions = require("gas-entry-generator").generate;
 
-describe('index.js', () => {
+describe("index.js", () => {
   beforeAll(async () => {
-    const entryFile = path.resolve(__dirname, 'test-source.js');
+    const entryFile = path.resolve(__dirname, "test-source.js");
 
     const bundler = new Bundler(entryFile, {
       watch: false,
       minify: true,
       sourceMaps: false,
-      outFile: 'bundle.js'
+      outFile: "bundle.js",
     });
 
     await parcelPluginGAS(bundler);
@@ -20,12 +20,14 @@ describe('index.js', () => {
     const bundle = await bundler.bundle();
   });
 
-  it('tests that function declarations are generated correctly', () => {
-    const bundlePath = path.resolve(__dirname, '../dist/bundle.js');
-    const bundleSource = fs.readFileSync(bundlePath, 'utf-8');
+  it("tests that function declarations are generated correctly", () => {
+    const bundlePath = path.resolve(__dirname, "../dist/bundle.js");
+    const bundleSource = fs.readFileSync(bundlePath, "utf-8");
 
-    const bundleSourceGAS = bundleSource.replace(/globalThis\./g, 'global.');
-    const entries = gasEntryGenerator(bundleSourceGAS).split('\n').join('');
+    const bundleSourceGAS = bundleSource.replace(/globalThis\./g, "global.");
+    const entries = generateEntryFunctions(bundleSourceGAS)
+      .entryPointFunctions.split("\n")
+      .join("");
 
     expect(bundleSource.includes(entries)).toBe(true);
   });
